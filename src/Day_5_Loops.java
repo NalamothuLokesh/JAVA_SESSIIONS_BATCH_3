fb3ba8d4a8aa679ab027baf5a6836e2615493b89
NalamothuLokesh <lokeshchowdarynalamothu@gmail.com>
2025-01-07 13:33:29 +0530

    Add files via upload

---
+++ src/Day_5_Loops.java
@@ -0,0 +1,10 @@
+public class Day_5_Loops {
+    public static void main(String[] args) {
+        String str = "Lokesh";
+        String a = str.substring(0,4);
+        System.out.println(a);
+        for(int i = 1; i <= 10;i++){
+            System.out.println("Loki");
+       }
+    }
+}
