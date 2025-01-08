66700eb9d6bd6fbe3d5cdcc87de5298ed265c06a
NalamothuLokesh <lokeshchowdarynalamothu@gmail.com>
2025-01-08 13:36:18 +0530

    Add files via upload

---
+++ src/Day_6_Problems.java
@@ -0,0 +1,10 @@
+public class Day_6_Problems {
+    public static void main(String[] args) {
+        //nested loop
+        for(int i = 0;i<=4;i++){//outer loop for rows.
+            for(int j = 0;j<i;j++){//inner loop for columns.
+                System.out.print("y ");
+            }System.out.println(" ");
+        }
+    }
+}
